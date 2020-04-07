5c0b7b4bf0d3c99abf104b2182fc7dc6fede6207
vaibhav naik <vaibhavnitya@gmail.com>
2020-04-07 22:52:23 +0530

    export user and usage modules

---
+++ electricModules.js
@@ -0,0 +1,7 @@
+const userModule = require('./electric/javascript/userModule')
+const usageModule = require('./electric/javascript/usageModule')
+
+module.exports = {
+  userModule,
+  usageModule
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+const blockchainModule = require('./electricModules')
+
+module.exports = blockchainModule
